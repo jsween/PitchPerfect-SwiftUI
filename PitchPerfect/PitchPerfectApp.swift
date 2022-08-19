80a3c0da136a6b850049c0f3e80a4158e117bd9b
Jon Sweeney <jonsweeney26@gmail.com>
2022-08-19 06:50:02 -0700

    Initial Commit

---
+++ PitchPerfect/PitchPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPerfectApp.swift
+//  PitchPerfect
+//
+//  Created by Jonathan Sweeney on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct PitchPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
